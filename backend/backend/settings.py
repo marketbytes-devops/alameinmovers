5c8da556d873824e84a7ad465cc97423ff3961af
ajay-marketbytes <ajay@marketbytes.in>
2025-06-05 13:29:05 +0530

    updated backend timout

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -188,10 +188,9 @@ REST_FRAMEWORK = {
     ),
 }
 
-# JWT settings
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(hours=12), 
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'AUTH_HEADER_TYPES': ('Bearer',),
